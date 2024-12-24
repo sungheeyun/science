4935840e69af1a84c94e7957e0be7f75f14a28e5
Sunghee Yun <sunghee.yun@gmail.com>
2024-12-23 23:18:29 -0800

    /simulation/input data/ add an YAML input file for 1d 2-body & 2-spring & 1-wall dynamics simulation

--- bin/sim-1d-2-bodies-1-wall.py
+++ bin/sim-1d-2-bodies-1-wall.py
@@ -20,21 +20,21 @@ from dynamics.utils import energy_info_text
 if __name__ == "__main__":
 
     # bodies
-    wall: VerticalWall1D = VerticalWall1D(-3.0)
-    ball_1: RigidBall = RigidBall(1.0, (-1, 0), (0, 0))
-    ball_2: RigidBall = RigidBall(1.0, (2, 0), (0, 0))
+    wall: VerticalWall1D = VerticalWall1D(-3)
+    ball_1: RigidBall = RigidBall(1, (-1, 0), (0, 0))
+    ball_2: RigidBall = RigidBall(1, (2, 0), (0, 0))
 
     bodies: Bodies = Bodies(ball_1, ball_2, wall)
 
     # forces
-    spring_1: Spring = Spring(10, 1.0, wall, ball_1)
-    spring_2: Spring = Spring(10, 1.0, ball_1, ball_2)
+    spring_1: Spring = Spring(10, 1, wall, ball_1)
+    spring_2: Spring = Spring(10, 1, ball_1, ball_2)
     friction: HorizontalFrictionalForce1D = HorizontalFrictionalForce1D(1, 3)
-    gravity: GravityLike = GravityLike([5.0, 0])
+    gravity: GravityLike = GravityLike([5, 0])
 
     forces: Forces = Forces(spring_1, spring_2, friction, gravity)
 
-    forces.approx_min_energy(bodies)
+    # forces.approx_min_energy(bodies)
 
     forces.register_forces(bodies)
 
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     bodies.add_objs(ax)
     forces.add_objs(ax)
 
-    ax.set_xlim(-3.0, 3.0)
-    ax.set_ylim(-1.0, 1.0)
+    ax.set_xlim(-3, 3)
+    ax.set_ylim(-1, 1)
     ax.set_aspect("equal")
     ax.grid(axis="x")
 
@@ -56,9 +56,6 @@ if __name__ == "__main__":
         pad=10,
     )
     ax.set_xlabel("x (m)")
-    ax.set_ylabel("potential energy (J)")
-
-    # ax.add_patch(ball)
 
     # Add a line to represent the path of motion
     ax.axhline(y=0, color="black", linestyle="-", alpha=0.3)
@@ -77,7 +74,7 @@ if __name__ == "__main__":
 
     def animate(frame):
         """Animation function"""
-        t = frame * 0.050  # Convert frame number to time (seconds)
+        t = frame * 0.040  # Convert frame number to time (seconds)
 
         bodies.update(t, forces)
         forces.update_objs()
@@ -94,7 +91,7 @@ if __name__ == "__main__":
 
     # Create animation
     anim = FuncAnimation(
-        fig, animate, init_func=init, frames=1000, interval=10, blit=True, repeat=False
+        fig, animate, init_func=init, frames=250, interval=40, blit=True, repeat=False
     )
 
     # writer = PillowWriter(fps=20)
